27ba078c40df1155ed49c9b2c516d0b0fdb86af2
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-03-29 15:42:29 +0530

    Update covid_epidemiology.sql

--- models/example/covid_epidemiology.sql
+++ models/example/covid_epidemiology.sql
@@ -1,53 +1,15 @@
-create  table "dev"."public"."covid_epidemiology__dbt_tmp"
-as (
-    select   
-        cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as "key",
-        cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as "date",
-        cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
-        cast(jsonb_extract_path_text("_airbyte_data",'new_deceased') as float) as new_deceased,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_tested') as float) as total_tested,
-        cast(jsonb_extract_path_text("_airbyte_data",'new_confirmed') as float) as new_confirmed,
-        cast(jsonb_extract_path_text("_airbyte_data",'new_recovered') as float) as new_recovered,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_deceased') as float) as total_deceased,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_confirmed') as float) as total_confirmed,
-        cast(jsonb_extract_path_text("_airbyte_data",'total_recovered') as float) as total_recovered
+create table "dev"."public"."covid_epidemiology__dbt_tmp"
+as
+(select
+        cast(json_extract_path_text("_airbyte_data",'key') as varchar) is not NULL as "key",
+        cast(json_extract_path_text("_airbyte_data",'date') as varchar) is not NULL as "date",
+        cast(json_extract_path_text("_airbyte_data",'new_tested') as float) is not NULL as new_tested,
+        cast(json_extract_path_text("_airbyte_data",'new_deceased') as float) is not NULL as new_deceased,
+        cast(json_extract_path_text("_airbyte_data",'total_tested') as float) is not NULL as total_tested,
+        cast(json_extract_path_text("_airbyte_data",'new_confirmed') as float)is not NULL  as new_confirmed,
+        cast(json_extract_path_text("_airbyte_data",'new_recovered') as float) is not NULL as new_recovered,
+        cast(json_extract_path_text("_airbyte_data",'total_deceased') as float) is not NULL as total_deceased,
+        cast(json_extract_path_text("_airbyte_data",'total_confirmed') as float) is not NULL as total_confirmed,
+        cast(json_extract_path_text("_airbyte_data",'total_recovered') as float) is not NULL as total_recovered
 from "dev".public._airbyte_raw_covid_epidemiology as table_alias
-);
-create view "dev"."public"."covid_epidemiology__dbt_tmp" as (
-    with parse_json_cte as (
-        select
-            cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as id,
-            cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as updated_at,
-            cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
-            cast(jsonb_extract_path_text("_airbyte_data",'new_deceased') as float) as new_deceased,
-            cast(jsonb_extract_path_text("_airbyte_data",'total_tested') as float) as total_tested,
-            cast(jsonb_extract_path_text("_airbyte_data",'new_confirmed') as float) as new_confirmed,
-            cast(jsonb_extract_path_text("_airbyte_data",'new_recovered') as float) as new_recovered,
-            cast(jsonb_extract_path_text("_airbyte_data",'total_deceased') as float) as total_deceased,
-            cast(jsonb_extract_path_text("_airbyte_data",'total_confirmed') as float) as total_confirmed,
-            cast(jsonb_extract_path_text("_airbyte_data",'total_recovered') as float) as total_recovered
-        from "dev".public._airbyte_raw_covid_epidemiology
-    ),
-    cte as (
-        select
-            *,
-            row_number() over (
-                partition by id
-                order by updated_at desc
-            ) as row_num
-        from parse_json_cte
-    )
-    select
-        substring(id, 1, 2) as id, -- Probably not the right way to identify the primary key in this dataset...
-        updated_at,
-        case when new_tested = 'NaN' then 0 else cast(new_tested as integer) end as new_tested,
-        case when new_deceased = 'NaN' then 0 else cast(new_deceased as integer) end as new_deceased,
-        case when total_tested = 'NaN' then 0 else cast(total_tested as integer) end as total_tested,
-        case when new_confirmed = 'NaN' then 0 else cast(new_confirmed as integer) end as new_confirmed,
-        case when new_recovered = 'NaN' then 0 else cast(new_recovered as integer) end as new_recovered,
-        case when total_deceased = 'NaN' then 0 else cast(total_deceased as integer) end as total_deceased,
-        case when total_confirmed = 'NaN' then 0 else cast(total_confirmed as integer) end as total_confirmed,
-        case when total_recovered = 'NaN' then 0 else cast(total_recovered as integer) end as total_recovered
-    from cte
-    where row_num = 1
-);
+ );
