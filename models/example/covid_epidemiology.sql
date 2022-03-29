f70c10532e77b3b0ea2032a8d9eab1aa2bdac121
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-03-29 14:48:51 +0530

    Update covid_epidemiology

--- models/example/covid_epidemiology.sql
+++ models/example/covid_epidemiology.sql
@@ -1,93 +1,60 @@
-  create table "dev"."public"."covid_epidemiology__dbt_tmp"
-      compound sortkey(_airbyte_emitted_at)
-  as (
+create  table "dev"."public"."covid_epidemiology__dbt_tmp"
+as (
+    select
+        _airbyte_emitted_at,
+        (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
 
-with __dbt__cte__covid_epidemiology_ab1 as (
-
--- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
--- depends_on: "dev".public._airbyte_raw_covid_epidemiology
-select
-    case when json_extract_path_text(_airbyte_data, 'key', true) != '' then json_extract_path_text(_airbyte_data, 'key', true) end as key,
-    case when json_extract_path_text(_airbyte_data, 'date', true) != '' then json_extract_path_text(_airbyte_data, 'date', true) end as date,
-    case when json_extract_path_text(_airbyte_data, 'new_tested', true) != '' then json_extract_path_text(_airbyte_data, 'new_tested', true) end as new_tested,
-    case when json_extract_path_text(_airbyte_data, 'new_deceased', true) != '' then json_extract_path_text(_airbyte_data, 'new_deceased', true) end as new_deceased,
-    case when json_extract_path_text(_airbyte_data, 'total_tested', true) != '' then json_extract_path_text(_airbyte_data, 'total_tested', true) end as total_tested,
-    case when json_extract_path_text(_airbyte_data, 'new_confirmed', true) != '' then json_extract_path_text(_airbyte_data, 'new_confirmed', true) end as new_confirmed,
-    case when json_extract_path_text(_airbyte_data, 'new_recovered', true) != '' then json_extract_path_text(_airbyte_data, 'new_recovered', true) end as new_recovered,
-    case when json_extract_path_text(_airbyte_data, 'total_deceased', true) != '' then json_extract_path_text(_airbyte_data, 'total_deceased', true) end as total_deceased,
-    case when json_extract_path_text(_airbyte_data, 'total_confirmed', true) != '' then json_extract_path_text(_airbyte_data, 'total_confirmed', true) end as total_confirmed,
-    case when json_extract_path_text(_airbyte_data, 'total_recovered', true) != '' then json_extract_path_text(_airbyte_data, 'total_recovered', true) end as total_recovered,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
-    getdate() as _airbyte_normalized_at
+        cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as "key",
+        cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as "date",
+        cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
+        cast(jsonb_extract_path_text("_airbyte_data",'new_deceased') as float) as new_deceased,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_tested') as float) as total_tested,
+        cast(jsonb_extract_path_text("_airbyte_data",'new_confirmed') as float) as new_confirmed,
+        cast(jsonb_extract_path_text("_airbyte_data",'new_recovered') as float) as new_recovered,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_deceased') as float) as total_deceased,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_confirmed') as float) as total_confirmed,
+        cast(jsonb_extract_path_text("_airbyte_data",'total_recovered') as float) as total_recovered
 from "dev".public._airbyte_raw_covid_epidemiology as table_alias
--- covid_epidemiology
-where 1 = 1
-),  __dbt__cte__covid_epidemiology_ab2 as (
+);
+create view "dev"."public"."covid_epidemiology__dbt_tmp" as (
+    with parse_json_cte as (
+        select
+            _airbyte_emitted_at,
 
--- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
--- depends_on: __dbt__cte__covid_epidemiology_ab1
-select
-    cast(key as varchar) as key,
-    cast(date as varchar) as date,
-    cast(new_tested as
-    float
-) as new_tested,
-    cast(new_deceased as
-    float
-) as new_deceased,
-    cast(total_tested as
-    float
-) as total_tested,
-    cast(new_confirmed as
-    float
-) as new_confirmed,
-    cast(new_recovered as
-    float
-) as new_recovered,
-    cast(total_deceased as
-    float
-) as total_deceased,
-    cast(total_confirmed as
-    float
-) as total_confirmed,
-    cast(total_recovered as
-    float
-) as total_recovered,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
-    getdate() as _airbyte_normalized_at
-from __dbt__cte__covid_epidemiology_ab1
--- covid_epidemiology
-where 1 = 1
-),  __dbt__cte__covid_epidemiology_ab3 as (
+            cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as id,
+            cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as updated_at,
+            cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
+            cast(jsonb_extract_path_text("_airbyte_data",'new_deceased') as float) as new_deceased,
+            cast(jsonb_extract_path_text("_airbyte_data",'total_tested') as float) as total_tested,
+            cast(jsonb_extract_path_text("_airbyte_data",'new_confirmed') as float) as new_confirmed,
+            cast(jsonb_extract_path_text("_airbyte_data",'new_recovered') as float) as new_recovered,
+            cast(jsonb_extract_path_text("_airbyte_data",'total_deceased') as float) as total_deceased,
+            cast(jsonb_extract_path_text("_airbyte_data",'total_confirmed') as float) as total_confirmed,
+            cast(jsonb_extract_path_text("_airbyte_data",'total_recovered') as float) as total_recovered
+        from "dev".public._airbyte_raw_covid_epidemiology
+    ),
+    cte as (
+        select
+            *,
+            row_number() over (
+                partition by id
+                order by updated_at desc
+            ) as row_num
+        from parse_json_cte
+    )
+    select
+        substring(id, 1, 2) as id, -- Probably not the right way to identify the primary key in this dataset...
+        updated_at,
+        _airbyte_emitted_at,
 
--- SQL model to build a hash column based on the values of this record
--- depends_on: __dbt__cte__covid_epidemiology_ab2
-select
-    md5(cast(coalesce(cast(key as varchar), '') || '-' || coalesce(cast(date as varchar), '') || '-' || coalesce(cast(new_tested as varchar), '') || '-' || coalesce(cast(new_deceased as varchar), '') || '-' || coalesce(cast(total_tested as varchar), '') || '-' || coalesce(cast(new_confirmed as varchar), '') || '-' || coalesce(cast(new_recovered as varchar), '') || '-' || coalesce(cast(total_deceased as varchar), '') || '-' || coalesce(cast(total_confirmed as varchar), '') || '-' || coalesce(cast(total_recovered as varchar), '') as varchar)) as _airbyte_covid_epidemiology_hashid,
-    tmp.*
-from __dbt__cte__covid_epidemiology_ab2 tmp
--- covid_epidemiology
-where 1 = 1
-)-- Final base SQL model
--- depends_on: __dbt__cte__covid_epidemiology_ab3
-select
-    key,
-    date,
-    new_tested,
-    new_deceased,
-    total_tested,
-    new_confirmed,
-    new_recovered,
-    total_deceased,
-    total_confirmed,
-    total_recovered,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
-    getdate() as _airbyte_normalized_at,
-    _airbyte_covid_epidemiology_hashid
-from __dbt__cte__covid_epidemiology_ab3
--- covid_epidemiology from "dev".public._airbyte_raw_covid_epidemiology
-where 1 = 1
-  );
+        case when new_tested = 'NaN' then 0 else cast(new_tested as integer) end as new_tested,
+        case when new_deceased = 'NaN' then 0 else cast(new_deceased as integer) end as new_deceased,
+        case when total_tested = 'NaN' then 0 else cast(total_tested as integer) end as total_tested,
+        case when new_confirmed = 'NaN' then 0 else cast(new_confirmed as integer) end as new_confirmed,
+        case when new_recovered = 'NaN' then 0 else cast(new_recovered as integer) end as new_recovered,
+        case when total_deceased = 'NaN' then 0 else cast(total_deceased as integer) end as total_deceased,
+        case when total_confirmed = 'NaN' then 0 else cast(total_confirmed as integer) end as total_confirmed,
+        case when total_recovered = 'NaN' then 0 else cast(total_recovered as integer) end as total_recovered
+    from cte
+    where row_num = 1
+);
