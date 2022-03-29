5e0dcfe74b51aa6b60ba300c50e199796b2c91b6
Bharath Reddy <89402199+Bharath-63@users.noreply.github.com>
2022-03-29 14:55:46 +0530

    Update covid_epidemiology.sql

--- models/example/covid_epidemiology.sql
+++ models/example/covid_epidemiology.sql
@@ -1,8 +1,6 @@
 create  table "dev"."public"."covid_epidemiology__dbt_tmp"
 as (
-    select
-        _airbyte_emitted_at,
-        
+    select   
         cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as "key",
         cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as "date",
         cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
@@ -18,8 +16,6 @@ from "dev".public._airbyte_raw_covid_epidemiology as table_alias
 create view "dev"."public"."covid_epidemiology__dbt_tmp" as (
     with parse_json_cte as (
         select
-            _airbyte_emitted_at,
-
             cast(jsonb_extract_path_text("_airbyte_data",'key') as varchar) as id,
             cast(jsonb_extract_path_text("_airbyte_data",'date') as varchar) as updated_at,
             cast(jsonb_extract_path_text("_airbyte_data",'new_tested') as float) as new_tested,
@@ -44,8 +40,6 @@ create view "dev"."public"."covid_epidemiology__dbt_tmp" as (
     select
         substring(id, 1, 2) as id, -- Probably not the right way to identify the primary key in this dataset...
         updated_at,
-        _airbyte_emitted_at,
-
         case when new_tested = 'NaN' then 0 else cast(new_tested as integer) end as new_tested,
         case when new_deceased = 'NaN' then 0 else cast(new_deceased as integer) end as new_deceased,
         case when total_tested = 'NaN' then 0 else cast(total_tested as integer) end as total_tested,
